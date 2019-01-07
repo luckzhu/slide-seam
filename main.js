a745fe4881a609417305f28912ffc74e5309378f
luckzhu <luckzhu1992@gmail.com>
2019-01-07 22:08:02 +0800

    轮播初实现

---
+++ main.js
@@ -0,0 +1,37 @@
+var allButtons = $('.buttons > li');
+var n=0;
+var size = allButtons.length;
+  
+$(allButtons).on('click',function(x){
+  
+  var index = $(x.currentTarget).index()
+  var p = index * -400
+  
+  $('.images').css({
+    transform: 'translateX(' + p + 'px)'
+  })
+  
+  n = index
+  
+  allButtons.eq(index)
+    .addClass('active')
+    .siblings('.active').removeClass('active')
+})
+
+
+var timerID = setInterval(() => {
+  n += 1
+  allButtons.eq(n % size).trigger('click')
+  },2000)
+
+
+$('.window').on('mouseenter', function(){
+  window.clearInterval(timerID)
+})
+
+$('.window').on('mouseleave', function(){
+  timerID = setInterval(() => {
+  n += 1
+  allButtons.eq(n % size).trigger('click')
+  },2000)
+})
